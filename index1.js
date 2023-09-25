e17fa03e694579bf3ab792c02741c6aa6b68dab2
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2023-09-25 14:56:26 +0200

    a

--- index1.js
+++ index1.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -10,7 +11,7 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
